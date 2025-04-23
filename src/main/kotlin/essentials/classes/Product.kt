10231013b69fe39166b262afd9cd96fcbd7bc00f
danielrotenberg <danirg10000@gmail.com>
2025-04-23 14:19:37 +0300

    * added Product.kt solution

--- src/main/kotlin/essentials/classes/Product.kt
+++ src/main/kotlin/essentials/classes/Product.kt
@@ -5,6 +5,25 @@ import kotlin.test.assertEquals
 
 // TODO
 
+class Product(val name: String, val price: Double, quantity: Int) {
+
+    var quantity: Int = quantity
+        set(value) {
+            field = if (value < 0) {
+                0
+            } else value
+        }
+
+    fun calculateTotalValue() = price * quantity
+
+    fun restock(num: Int) {
+        if (num > 0) {
+            quantity += num
+        }
+    }
+
+}
+
 fun main() {
     //val laptop = Product("Laptop", 999.99, 5)
     //
@@ -29,41 +48,41 @@ fun main() {
 }
 
 class ProductTest {
-//   @Test
-//   fun `should keep name, price and quantity`() {
-//       val product = Product("Apple", 1.0, 10)
-//       assertEquals("Apple", product.name)
-//       assertEquals(1.0, product.price)
-//       assertEquals(10, product.quantity)
-//   }
-// 
-//   @Test
-//   fun `should calculate total value`() {
-//       assertEquals(10.0, Product("Apple", 1.0, 10).calculateTotalValue())
-//       assertEquals(0.0, Product("Apple", 1.0, 0).calculateTotalValue())
-//       assertEquals(0.0, Product("Apple", 0.0, 10).calculateTotalValue())
-//       assertEquals(4.8, Product("Apple", 1.2, 4).calculateTotalValue())
-//   }
-// 
-//   @Test
-//   fun `should restock`() {
-//       val product = Product("Apple", 1.0, 10)
-//       product.restock(5)
-//       assertEquals(15, product.quantity)
-//       product.restock(-5)
-//       assertEquals(15, product.quantity)
-//       product.restock(0)
-//       assertEquals(15, product.quantity)
-//   }
-// 
-//   @Test
-//   fun `should not allow negative quantity`() {
-//       val product = Product("Apple", 1.0, 10)
-//       product.quantity = -5
-//       assertEquals(0, product.quantity)
-//       product.quantity = 8
-//       assertEquals(8, product.quantity)
-//       product.quantity = 0
-//       assertEquals(0, product.quantity)
-//   }
+    @Test
+    fun `should keep name, price and quantity`() {
+        val product = Product("Apple", 1.0, 10)
+        assertEquals("Apple", product.name)
+        assertEquals(1.0, product.price)
+        assertEquals(10, product.quantity)
+    }
+
+    @Test
+    fun `should calculate total value`() {
+        assertEquals(10.0, Product("Apple", 1.0, 10).calculateTotalValue())
+        assertEquals(0.0, Product("Apple", 1.0, 0).calculateTotalValue())
+        assertEquals(0.0, Product("Apple", 0.0, 10).calculateTotalValue())
+        assertEquals(4.8, Product("Apple", 1.2, 4).calculateTotalValue())
+    }
+
+    @Test
+    fun `should restock`() {
+        val product = Product("Apple", 1.0, 10)
+        product.restock(5)
+        assertEquals(15, product.quantity)
+        product.restock(-5)
+        assertEquals(15, product.quantity)
+        product.restock(0)
+        assertEquals(15, product.quantity)
+    }
+
+    @Test
+    fun `should not allow negative quantity`() {
+        val product = Product("Apple", 1.0, 10)
+        product.quantity = -5
+        assertEquals(0, product.quantity)
+        product.quantity = 8
+        assertEquals(8, product.quantity)
+        product.quantity = 0
+        assertEquals(0, product.quantity)
+    }
 }
